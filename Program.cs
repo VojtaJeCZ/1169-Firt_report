834add8bcb94dc11fef976c889722b04069a4fc4
VojtaJeCZ <1169@student.itgymnazium.cz>
2023-11-09 11:42:45 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,127 @@
+﻿//int a = 25;
+
+//a = a + 4;
+
+//Console.WriteLine(a);
+
+//int b = 8;
+
+//b = b + 1;
+
+//Console.WriteLine(b);
+
+//int input1 = 2;
+//int input2 = 3;
+
+//input1* input2;
+
+
+
+
+
+//int input = 13 + 1;
+//if ((input1 % 2) == 0)
+//{
+//    input1 *= 3;
+//}
+//else
+//{
+//    input1 *= 2;
+//}
+//Console.WriteLine(input1);
+
+//string input2 = "Pepa";
+//string s = "Ahoj, ";
+//s += "Vojto!";
+
+//Console.WriteLine(s);
+
+//string x = "";
+//if (string.IsNullOrWhiteSpace(x))
+//    //{
+//    //    Console.WriteLine("prázdné");
+
+//    //}
+//    //else
+//    //{
+//    //    Console.WriteLine("něco tam je");
+//    //}
+
+//    //string input3 = "vávra vavrá"
+//    //if (input3.Length > 5 && input3.Lengh < 20)  
+
+
+
+
+//    int input6 = 3;
+
+//if (input6 > 0)
+//{
+//    if ((input6 % 2) == 0)
+//    {
+//        Console.WriteLine("Číslo je sudé.");
+//    }
+//    else
+//    {
+//        Console.WriteLine("Číslo je liché");
+//    }
+//}
+//else
+//{
+//    Console.WriteLine("Číslo nejde modulovat.");
+//}
+
+
+//bool input3 = false;
+//if (input3)
+//{
+//    Console.WriteLine("Zadal si špatně.");
+//}
+//else
+//{
+//    Console.WriteLine("Zadal si správně.");
+//}
+
+
+
+//int input4 = 25;
+//int input5 = 25;
+//if (input4 == input5)
+//{
+//    Console.WriteLine("čísla jsou stejná");
+//}
+//if (input4 > input5)
+//{
+//    Console.WriteLine("input4 je větší");
+//}
+//else if (input4 < input5)
+//{
+//    Console.WriteLine("input4 je menší");
+//}
+//else
+//{
+//    Console.WriteLine("čísla jsou stejná");
+//}
+
+
+
+if (input < 1)
+{
+    Console.WriteLine("neexistuje");
+}
+if (input > 5)
+{
+    Console.WriteLine("neexistující známka");
+}
+if (input <= 4)
+{
+    Console.WriteLine("jsem ok.");
+}
+else
+{
+    Console.WriteLine("Jsem v hájí!");
+}
+
+
+
+
